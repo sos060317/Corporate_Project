b5470527aef9a00aa28ad84fe58848fcd2e409ec
Chosungwoo123 <ejyoo12345@naver.com>
2023-08-14 23:30:03 +0900

    Make a Enemy Targeting System.

---
+++ Assets/Scripts/TEST/TargetTest.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetTest : MonoBehaviour
+{
+    [HideInInspector] public bool targeting = false;
+}
