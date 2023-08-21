f307be736055ff2276a53546eb81364bcdaec929
Chosungwoo123 <ejyoo12345@naver.com>
2023-08-21 21:30:35 +0900

    Make Info Animation.

--- Assets/Scripts/UI/InGame/NextWaveInfo.cs
+++ Assets/Scripts/UI/InGame/NextWaveInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,21 @@ using UnityEngine.UI;
 public class NextWaveInfo : MonoBehaviour
 {
     [SerializeField] private Text infoText;
+    [SerializeField] private Image infoBg;
+
+    private float fadeAmount = 0f;
+
+    private void Start()
+    {
+        Color infoTextColor = infoText.color;
+        Color infoBgColor = infoBg.color;
+
+        infoTextColor.a = 0f;
+        infoBgColor.a = 0f;
+        
+        infoText.color = infoTextColor;
+        infoBg.color = infoBgColor;
+    }
 
     public void InitInfo(List<string> enemyNameList, List<int> enemyCountList)
     {   
@@ -20,5 +36,65 @@ public class NextWaveInfo : MonoBehaviour
                 infoText.text += "\n";
             }
         }
+    }
+
+    private void OnMouseEnter()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ShowInfo());
+    }
+
+    private void OnMouseExit()
+    {
+        StopAllCoroutines();
+        StartCoroutine(HideInfo());
+    }
+
+    private IEnumerator ShowInfo()
+    {
+        Color infoTextColor = infoText.color;
+        Color infoBgColor = infoBg.color;
+
+        float textAlpha = 1f;
+        float bgAlpha = 0.4f;
+
+        while (fadeAmount <= 1)
+        {
+            infoTextColor.a = fadeAmount * textAlpha;
+            infoBgColor.a = fadeAmount * bgAlpha;
+
+            infoText.color = infoTextColor;
+            infoBg.color = infoBgColor;
+
+            fadeAmount += Time.deltaTime * 4;
+
+            yield return null;
+        }
+
+        fadeAmount = 1f;
+    }
+    
+    private IEnumerator HideInfo()
+    {
+        Color infoTextColor = infoText.color;
+        Color infoBgColor = infoBg.color;
+
+        float textAlpha = 1f;
+        float bgAlpha = 0.4f;
+
+        while (fadeAmount >= 0)
+        {
+            infoTextColor.a = fadeAmount * textAlpha;
+            infoBgColor.a = fadeAmount * bgAlpha;
+
+            infoText.color = infoTextColor;
+            infoBg.color = infoBgColor;
+
+            fadeAmount -= Time.deltaTime * 4;
+
+            yield return null;
+        }
+
+        fadeAmount = 0f;
     }
 }
