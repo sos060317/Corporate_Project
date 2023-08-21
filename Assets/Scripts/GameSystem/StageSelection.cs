305ef22750b0aeaff9afeb0ccea69f4de91e6270
woojiwon <woojiwon060317@gmail.com>
2023-08-21 23:45:14 +0900

    Make Part of The Stage Selection

---
+++ Assets/Scripts/GameSystem/StageSelection.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.UI;
+
+// 선택한 스테이지로 이동
+public class StageSelection : MonoBehaviour
+{
+    [Header("스테이지")]
+    [SerializeField] private bool unlocked;      // 스테이지가 풀렸는지 아닌지를 판별하는 변수
+    [SerializeField] private Image unlockImage;  // 스테이지가 잠겼는지 알려주는 이미지
+    [SerializeField] private GameObject[] stars; // 스테이지의 클리어 정도를 알려주는 별
+
+    [Header("씬 로딩")]
+    [SerializeField] private SceneLoader sceneLoader;
+
+    private void Update()
+    {
+        UpdateLevelImage();
+    }
+
+    // 스테이지 클리어 여부를 알려주는 함수
+    private void UpdateLevelImage()
+    {
+        if (!unlocked) // 만약 스테이지를 클리어 하지 못헸다면
+        {
+            unlockImage.gameObject.SetActive(true); // 잠긴 이미지를 보여주고
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i].gameObject.SetActive(false); // 별 이미지를 없애준다
+            }
+        }
+        else // 스테이지를 클리어 했다면
+        {
+            unlockImage.gameObject.SetActive(false); // 잠긴 이미지를 없애주고
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i].gameObject.SetActive(true); // 별 이미지를 보여준다
+            }
+        }
+    }
+
+    // 스테이지를 선택했을 때의 씬 이동
+    public void PressSelection(int stageId)
+    {
+        if (unlocked)
+        {
+            sceneLoader.GetComponent<SceneLoader>().LoadScene(stageId + 1); // 로딩 창 불러오기
+        }
+    }
+}
