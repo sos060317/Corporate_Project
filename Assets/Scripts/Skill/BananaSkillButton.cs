63f32a5517d858c507d09bebc8020f1503a6e21f
chosungwoo <ejyoo12345@naver.com>
2023-10-17 20:59:50 +0900

    Make a Banana Skill Button.

---
+++ Assets/Scripts/Skill/BananaSkillButton.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BananaSkillButton : MonoBehaviour
+{
+    [SerializeField] private float skillCooldownTime;
+    [SerializeField] private Image skillImage;
+    [SerializeField] private GameObject bananaSkillPrefab;
+
+    private float skillTime;
+
+    private Button skillButton;
+
+    private void Awake()
+    {
+        skillButton = GetComponent<Button>();
+
+        skillTime = skillCooldownTime;
+    }
+
+    private void Update()
+    {
+        SKillCooldownUpdate();
+    }
+
+    private void SKillCooldownUpdate()
+    {
+        if (skillTime >= skillCooldownTime)
+        {
+            skillButton.interactable = true;
+            
+            return;
+        }
+
+        skillTime += Time.deltaTime;
+
+        skillImage.fillAmount = skillTime / skillCooldownTime;
+    }
+
+    public void UseBananaSkill()
+    {
+        skillButton.interactable = false;
+
+        skillTime = 0;
+        
+        Instantiate(bananaSkillPrefab, Vector3.zero, Quaternion.identity);
+    }
+}
