d7f974b21501eb8b7f40600a68ba576ccf26a0c1
chosungwoo <ejyoo12345@naver.com>
2023-10-17 20:14:59 +0900

    Make a SkillCoolTime.

--- Assets/Scripts/Skill/MeteorSkillButton.cs
+++ Assets/Scripts/Skill/MeteorSkillButton.cs
@@ -1,13 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MeteorSkillButton : MonoBehaviour
 {
+    [SerializeField] private float skillCooldownTime;
+    [SerializeField] private Image skillImage;
     [SerializeField] private GameObject meteorSkillPrefab;
+
+    private float skillTime;
+
+    private Button skillButton;
+
+    private void Awake()
+    {
+        skillButton = GetComponent<Button>();
+
+        skillTime = skillCooldownTime;
+    }
+
+    private void Update()
+    {
+        SKillCooldownUpdate();
+    }
+
+    private void SKillCooldownUpdate()
+    {
+        if (skillTime >= skillCooldownTime)
+        {
+            skillButton.interactable = true;
+            
+            return;
+        }
+
+        skillTime += Time.deltaTime;
+
+        skillImage.fillAmount = skillTime / skillCooldownTime;
+    }
 
     public void UseMeteorSkill()
     {
+        skillButton.interactable = false;
+
+        skillTime = 0;
+        
         Instantiate(meteorSkillPrefab, Vector3.zero, Quaternion.identity);
     }
 }
