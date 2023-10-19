17a3efd75877888709390ba4a5fb4e62783bbea7
chosungwoo <ejyoo12345@naver.com>
2023-10-19 18:09:57 +0900

    Make a TowerMeun Button in Stage0.

--- Assets/Scripts/TEST/TowerNodeBuildUI.cs
+++ Assets/Scripts/TEST/TowerNodeBuildUI.cs
@@ -12,21 +12,40 @@ public class TowerNodeBuildUI : MonoBehaviour
     public Spawner UTower;
 
     public Image towerWindow;
+    public RectTransform buttonImage;
 
     private RectTransform rectTransform;
+
+    private bool isShowWindow = false;
 
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
     }
 
+    public void ButtonShowFunction()
+    {
+        if (isShowWindow)
+        {
+            TowerBuildManager.Instance.HideTowerWindow();
+        }
+        else
+        {
+            ShowTowerWindow(null);
+        }
+    }
+
     public void ShowTowerWindow(TowerNode towerNode)
     {
+        isShowWindow = true;
+        buttonImage.rotation = Quaternion.Euler(0, 0, 180f);
         rectTransform.DOAnchorPosY(0, 0.3f).SetEase(Ease.Linear);
     }
 
     public void HideTowerWindow()
     {
-        rectTransform.DOAnchorPosY(-500, 0.3f).SetEase(Ease.Linear);
+        isShowWindow = false;
+        buttonImage.rotation = Quaternion.Euler(0, 0, 0f);
+        rectTransform.DOAnchorPosY(-390, 0.3f).SetEase(Ease.Linear);
     }
 }
