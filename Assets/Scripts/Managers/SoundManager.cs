694fb9fcd8949e1bf147a841d6d2136bc2545032
chosungwoo <ejyoo12345@naver.com>
2023-10-20 20:34:31 +0900

    Make a SoundManager.

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    #region 싱글톤
-
     private static SoundManager instance = null;
+
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private GameObject soundEffectObj;
+
+    public float sfxVolume = 1f;
+    public float bgmVolume = 1f;
 
     public static SoundManager Instance
     {
@@ -22,22 +26,82 @@ public class SoundManager : MonoBehaviour
         }
     }
 
-    #endregion
-
-    public float sfxVolume = 1f;
-    public float bgmVolume = 1f;
-
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
-            
             DontDestroyOnLoad(instance);
         }
         else
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Start()
+    {
+        musicSource.loop = true;
+    }
+
+    public void PlaySound(AudioClip clip, float pitch)
+    {
+        //GameObject soundObj = PoolManager.Instance.GetGameObejct(soundEffectObj, transform.position, Quaternion.identity);
+
+        GameObject soundObj = Instantiate(soundEffectObj, transform.position, Quaternion.identity);
+        
+        soundObj.GetComponent<AudioSource>().pitch = pitch;
+        soundObj.GetComponent<AudioSource>().volume = sfxVolume;
+
+        soundObj.SetActive(true);
+        
+        soundObj.GetComponent<AudioSource>().PlayOneShot(clip);
+
+        StartCoroutine(StopSound(soundObj, clip.length));
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        //GameObject soundObj = PoolManager.Instance.GetGameObejct(soundEffectObj, transform.position, Quaternion.identity);
+
+        GameObject soundObj = Instantiate(soundEffectObj, transform.position, Quaternion.identity);
+        
+        soundObj.GetComponent<AudioSource>().pitch = 1f;
+        soundObj.GetComponent<AudioSource>().volume = sfxVolume;
+
+        soundObj.SetActive(true);
+        
+        soundObj.GetComponent<AudioSource>().PlayOneShot(clip);
+
+        StartCoroutine(StopSound(soundObj, clip.length));
+    }
+    
+    IEnumerator StopSound(GameObject soundObj, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (soundObj != null)
+        {
+            soundObj.SetActive(false);
+        }
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.volume = bgmVolume;
+        musicSource.Play();
+    }
+    
+    public void SetMusicVolume(float value)
+    {
+        bgmVolume = value;
+        
+        musicSource.volume = value;
+    }
+    
+    public void SetEffectVolume(float value)
+    {
+        sfxVolume = value;
     }
 }
