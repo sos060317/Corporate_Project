07035af0f63ae5afbaa75f0528ad16d39ffc71c3
WooJiWon <woojiwon060317@gmail.com>
2023-10-26 20:56:46 +0900

    Create Stage Enemy Characteristics

---
+++ Assets/Scripts/Enemy/StageEnemyCharacteristicsSO.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StageEnemyCharacteristics", menuName = "Scriptable Objects/Stage/StageEnemyCharacteristics")]
+public class StageEnemyCharacteristicsSO : ScriptableObject
+{
+    public string enemyName;
+
+    #region Header
+
+    [Space(10)]
+    [Header("추가 기본 스탯")]
+
+    #endregion
+
+    public float enemyBaseHealth;
+
+    public float enemyBaseMoveSpeed;
+
+    public float enemyBaseAttackRate;
+
+    public float enemyBaseAttackRange;
+
+    #region Header
+
+    [Space(10)]
+    [Header("추가 방어력")]
+
+    #endregion
+
+    [Range(0f, 100f)]
+    public float defense;
+
+    [Range(0f, 100f)]
+    public float magicResistance;
+
+    #region Header
+
+    [Space(10)]
+    [Header("추가 공격력")]
+
+    #endregion
+
+    public float attackPower;
+
+    public float spellPower;
+
+    #region Header
+
+    [Space(10)]
+    [Header("UI 관련")]
+
+    #endregion
+
+    public Sprite profileImage;
+
+    #region Header
+
+    [Space(10)]
+    [Header("코인 감소")]
+
+    #endregion
+
+    public int coins;
+}
